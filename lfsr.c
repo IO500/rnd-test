f4ca5fa3d62d8b8541673b2409f0a179cdaf488a
Jay Lofstead <gflofst@sandia.gov>
2026-08-21 22:11:06 -0600

    code now works for 3 to 34 bits wide

--- lfsr.c
+++ lfsr.c
@@ -18,39 +18,41 @@ typedef struct {
 // https://ww2.ams.org/journals/mcom/1973-27-124/S0025-5718-1973-0327722-7/S0025-5718-1973-0327722-7.pdf?t=1779476860989
 LFSRConfig lfsr_configs[] =
 {
-    {0, 1, {0}},               // 0
-    {1, 1, {0}},               // 1
-    {2, 2, {2,1}},             // 2
-    {3, 2, {3,2}},             // 3
-    {4 ,2, {4,3}},             // 4
-    {5, 4, {5,4,3,2}},         // 5
-    {6, 4, {6,5,3,2}},         // 6
-    {7, 4, {7,6,5,4}},         // 7
-    {8, 4, {8,6,5,4}},         // 8
-    {9, 4, {9,8,6,5}},         // 9
-    {10, 4, {10,9,7,6}},       // 10
-    {11, 4, {11,10,9,7}},      // 11
-    {12, 4, {12,11,8,6}},      // 12
-    {13, 4, {13,12,10,9}},     // 13
-    {14, 4, {14,13,11,9}},     // 14
-    {15, 4, {15,14,13,11}},    // 15
-    {16, 4, {16,14,13,11}},    // 16
-    {17, 4, {17,16,15,14}},    // 17
-    {18, 4, {18,17,16,13}},    // 18
-    {19, 4, {19,18,17,14}},    // 19
-    {20, 4, {20,19,16,14}},    // 20
-    {21, 4, {21,20,19,16}},    // 21
-    {22, 4, {22,19,18,17}},    // 22
-    {23, 4, {23,22,20,18}},    // 23
-    {24, 4, {24,23,21,20}},    // 24
-    {25, 4, {25,24,23,22}},    // 25
-    {26, 4, {26,25,24,20}},    // 26
-    {27, 4, {27,26,25,22}},    // 27
-    {28, 4, {28,27,24,22}},    // 28
-    {29, 4, {29,28,27,25}},    // 29
-    {30, 4, {30,29,26,24}},    // 30
-    {31, 4, {31,30,29,28}},    // 31
-    {32, 4, {32,30,26,25}}     // 32
+    {0, 1, {0}},               // 0 dummy
+    {1, 1, {0}},               // 1 dummy
+    {2, 2, {1,0}},             // 2 dummy
+    {3, 2, {2,1}},             // 3 validated
+    {4 ,2, {3,2}},             // 4 validated
+    {5, 2, {4,2}},             // 5 validated
+    {6, 4, {5,3,2,0}},         // 6 validated
+    {7, 2, {6,2}},             // 7 validated
+    {8, 4, {7,3,2,1}},         // 8 validated
+    {9, 2, {8,4}},             // 9 validated
+    {10, 2, {9,2}},            // 10 validated
+    {11, 2, {10,1}},           // 11 validated
+    {12, 4, {11,5,3,0}},       // 12 validated
+    {13, 4, {12,11,9,8}},     // 13
+    {14, 4, {13,12,10,8}},     // 14
+    {15, 2, {14,0}},    // 15
+    {16, 4, {15,11,2,0}},    // 16
+    {17, 2, {16,2}},    // 17
+    {18, 2, {17,6}},    // 18
+    {19, 4, {18,17,16,12}},    // 19
+    {20, 2, {19,2}},    // 20
+    {21, 2, {20,1}},    // 21
+    {22, 2, {21,0}},    // 22
+    {23, 2, {22,4}},    // 23
+    {24, 4, {23,6,1,0}},    // 24
+    {25, 2, {24,2}},    // 25
+    {26, 4, {25,24,23,19}},    // 26
+    {27, 4, {26,25,24,21}},    // 27
+    {28, 2, {27,2}},    // 28
+    {29, 2, {28,1}},    // 29
+    {30, 4, {29,28,25,23}},    // 30
+    {31, 2, {30,2}},    // 31
+    {32, 4, {31,30,29,9}},     // 32
+    {33, 2, {32, 12}},               // 33
+    {34, 4, {33,32,31,6}}                // 34
 };
 
 #define NUM_LFSR_CONFIGS (sizeof(lfsr_configs)/sizeof(lfsr_configs[0]))
@@ -62,6 +64,13 @@ typedef struct {
     uint8_t state;  // lower 6 bits used, range 0..63
 } LFSR6;
 
+typedef struct
+{
+    uint8_t bits;
+    uint64_t state;
+    uint64_t mask;
+} LFSR;
+
 // Initialize with nonzero 6-bit seed
 void lfsr6_init(LFSR6 *lfsr, uint8_t seed) {
     seed &= LFSR_MASK;
@@ -71,6 +80,21 @@ void lfsr6_init(LFSR6 *lfsr, uint8_t seed) {
     }
 
     lfsr->state = seed;
+}
+
+void lfsr_init (LFSR * lfsr, uint8_t bits, uint64_t seed)
+{
+    lfsr->bits = bits;
+    lfsr->mask = ((1ull << bits) - 1u);
+    lfsr->state = seed & lfsr->mask;
+
+    printf ("bits: %hhu mask: %llu state: %llu\n", lfsr->bits, lfsr->mask, lfsr->state);
+    if (lfsr_configs [bits].num_taps >= 2)
+    printf ("taps [0]: %d taps [1]: %d", lfsr_configs [lfsr->bits].taps [0], lfsr_configs [lfsr->bits].taps [1]);
+    if (lfsr_configs [bits].num_taps == 4)
+    printf (" taps [2]: %d taps [3]: %d\n", lfsr_configs [lfsr->bits].taps [2], lfsr_configs [lfsr->bits].taps [3]);
+    else
+    printf ("\n");
 }
 
 // 6-bit, 4-tap, left-shifting maximal LFSR
@@ -91,15 +115,45 @@ uint8_t lfsr6_step(LFSR6 *lfsr) {
     return lfsr->state;
 }
 
-int main(void) {
+uint64_t lfsr_step (LFSR * lfsr)
+{
+    uint64_t s = lfsr->state;
+    uint8_t feedback = 0;
+
+    switch (lfsr_configs [lfsr->bits].num_taps)
+    {
+        case 2:
+            feedback =
+                ((s >> lfsr_configs [lfsr->bits].taps [0]) ^
+                 (s >> lfsr_configs [lfsr->bits].taps [1])
+                )
+                & 1u;
+            break;
+        case 4:
+            feedback =
+                ((s >> lfsr_configs [lfsr->bits].taps [0]) ^
+                 (s >> lfsr_configs [lfsr->bits].taps [1]) ^
+                 (s >> lfsr_configs [lfsr->bits].taps [2]) ^
+                 (s >> lfsr_configs [lfsr->bits].taps [3])
+                )
+                & 1u;
+            break;
+        default:
+            printf ("ERROR: %d\n", lfsr->bits);
+    }
+
+    lfsr->state = ((s << 1) & lfsr->mask) | feedback;
+
+    return lfsr->state;
+}
+
+int old_main(void) {
     LFSR6 lfsr;
-    lfsr6_init(&lfsr, 1);
+    uint8_t initial = 1;
+    lfsr6_init(&lfsr, initial);
 
-    uint8_t initial = lfsr.state;
-
-    int count = 0;
-
-    do {
+    do
+    {
         printf("%2u\n", lfsr.state);
 
         lfsr6_step(&lfsr);
@@ -143,14 +197,14 @@ void generate_lfsr_sequence(const LFSRConfig *config)
     fclose (f);
 }
 
-int main2()
+int main (void)
 {
     // The min and max 4KB blocks based on file sizes driven by a network rate and 300 seconds
     uint64_t min = 1024l * 1024 * 300 / 4096; // 1 MB/sec for 300 seconds
     uint64_t max = 1024l * 1024 * 1024 * 200 * 300 / 4096; // 200 GB/sec for 300 seconds
-    uint32_t min_bits = 0;
-    uint32_t max_bits = 0;
-    uint32_t bitmask = 1;
+    uint8_t min_bits = 0;
+    uint8_t max_bits = 0;
+    uint64_t seed = 1;//0xffff; // start with lots of 1 bits
 
     uint64_t t = min;
     while (t >>= 1) min_bits++;
@@ -158,15 +212,25 @@ int main2()
     while (t >>= 1) max_bits++;
 
     printf ("min(bits): %llu(%u) max(bits): %llu(%u)\n", min, min_bits, max, max_bits);
+    LFSR lfsr;
 
-    uint32_t a = 3401712413U;//214013U;
-    uint32_t c = 2531011U;
-
-    //for (int i = 0; i < NUM_LFSR_CONFIGS; i++)
-    //for (int i = 0; i < 10; i++)
-    int i = 3;
+    size_t buf_len = 256;
+    char buf [buf_len];
+    for (uint8_t i = 3; i < NUM_LFSR_CONFIGS; i++)
+    //for (int i = 3; i < 10; i++)
+    //uint8_t i = 12;
     {
-        generate_lfsr_sequence(&lfsr_configs[i]);
+        lfsr_init (&lfsr, i, seed);
+        snprintf (buf, buf_len, "%u.txt", lfsr.bits);
+        FILE * f = fopen (buf, "w");
+        do
+        {
+            //printf("%llu\n", lfsr.state);
+            fprintf(f, "%llu\n", lfsr.state);
+    
+            lfsr_step(&lfsr);
+        } while (lfsr.state != seed);
+        fclose (f);
     }
     return 0;
 }
